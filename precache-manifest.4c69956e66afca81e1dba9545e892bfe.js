160087b9c1b51e19a2ca99d7f2bb3601c4fc3c80
Anton Kostylov <antoniok003@gmail.com>
2021-05-17 13:08:51 +0300

    Updates

---
+++ precache-manifest.4c69956e66afca81e1dba9545e892bfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38cb19f81c9106f00beeeba5aeb4bfc5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9700694037c69f7642e3",
+    "url": "./static/css/main.cab80236.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9700694037c69f7642e3",
+    "url": "./static/js/main.8d8e4dad.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
